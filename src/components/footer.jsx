3064ad268360d72c7bfa9d6e7d08c659015d59cd
Parth Sharma <parthsharma20@gnu.ac.in>
2023-04-29 15:09:50 +0530

    FOOTER

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,51 +1,52 @@
 import { GitHub, Twitter } from './Icons'
 
 
-const footerLinks = [
-  {
-    id: 1,
-    link: "",
-    name: "GitHub",
-    icon: <GitHub />,
-  },
-  {
-    id: 2,
-    link: "",
-    name: "Twitter",
-    icon: <Twitter />,
-  },
-]
+// const footerLinks = [
+//   {
+//     id: 1,
+//     link: "",
+//     name: "GitHub",
+//     icon: <GitHub />,
+//   },
+//   {
+//     id: 2,
+//     link: "",
+//     name: "Twitter",
+//     icon: <Twitter />,
+//   },
+// ]
 
-const Footer = () => {
-  return (
-    <div className="footer">
-      <div>
-        <p>
-          Crafted with ❤️ by
-          <a
-            href="https://github.com/hack-parthsharma"
-            target="_blank"
-            className="footer-link"
-            rel="noreferrer"
-          >
-            {' '}
-            Hiren and Parth Sharma
-          </a>
-        </p>
-      </div>
-      <div className="footer">
-            <a
-              href="https://twitter.com/hirensharmaji"
-              target="_blank"
-              rel="noreferrer"
-              className="footer-link"
-            >
-            {' '}
-            Hiren and Parth Sharma
-            </a>
-        ))
-      </div>
-    </div>
-  )
-}
+// const Footer = () => {
+//   return (
+//     <div className="footer">
+//       <div>
+//         <p>
+//           Crafted with ❤️ by
+//           <a
+//             href="https://github.com/hack-parthsharma"
+//             target="_blank"
+//             className="footer-link"
+//             rel="noreferrer"
+//           >
+//             {' '}
+//             Hiren and Parth Sharma
+//           </a>
+//         </p>
+//       </div>
+//       <div className="footer">
+//             <a
+//               href="https://twitter.com/hirensharmaji"
+//               target="_blank"
+//               rel="noreferrer"
+//               className="footer-link"
+//             >
+//             {' '}
+//             Hiren and Parth Sharma
+//             </a>
+//         ))
+//       </div>
+//     </div>
+//   )
+// }
+<p> Created By Parth and Hiren Sharma</p>
 export default Footer
